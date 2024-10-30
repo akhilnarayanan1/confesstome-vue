af4572a180e7b93e466df1d205f762bf590b7ac1
Akhil Narayanan <akhil.narayanan@se.com>
2024-10-30 17:36:10 +0530

    🚨 fix alert

--- composables/toasts.ts
+++ composables/toasts.ts
@@ -4,16 +4,17 @@ import _ from "lodash";
 export const toasts = () => useState<ToastData[]>("toasts", () => []);
 
 export const addToast = (toast: ToastData) => {
-    toast.id = toasts().value.length;
-    toast.duration ? setTimeout(() => {
-        const isOnIndex = (_.findIndex(toasts().value, {id: toast.id}));
-        toasts().value.splice(isOnIndex, 1);
-    }, toast.duration) : null;
-    toasts().value.push(toast);
-    if (_.findIndex(toasts().value, 'duration') < 0) {
-        toasts().value = toasts().value.slice(-1);
+    toast.id = (toasts().value.length + 1); // auto populate IDs
+    // set timeout for toast duration parameter if found
+    if (toast.duration) {
+        setTimeout(() => {
+            const isOnIndex = (_.findIndex(toasts().value, {id: toast.id}));
+            toasts().value.splice(isOnIndex, 1);
+        }, toast.duration)
     };
-};
+    // load everthing to state
+    toasts().value.push(toast);
+}
 
 export const getToasts = () => computed(()=>toasts()).value;
 
